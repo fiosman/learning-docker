a03a734519023c7a713843c3431590c2e761d227
fiosman <fares.osman@outlook.com>
2023-04-24 23:32:15 -0400

    adds bash script to build the docker image and container

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Stop and remove any existing container with the same name
+docker stop learndocker && docker rm learndocker
+
+# Build the Docker image
+docker build -t learndocker .
+
+# Run the Docker container
+docker run -d --name learndocker -p 3000:3000 learndocker
